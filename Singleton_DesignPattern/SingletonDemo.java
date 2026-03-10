013f81343a5256c25aa2cf67a7aa5b3083549b77
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2026-03-10 23:39:27 +0530

    "SingletonDP"

---
+++ Singleton_DesignPattern/SingletonDemo.java
@@ -0,0 +1,23 @@
+package Singleton_DesignPattern;
+
+
+class Singleton{
+  static Singleton instance = new Singleton();
+  private Singleton(){
+
+  }
+  public static Singleton getSingleton(){
+    return instance;
+  }
+}
+public class SingletonDemo {
+  public static void main(String[] args) {
+    Singleton instance1 = Singleton.getSingleton();
+    Singleton instance2 = Singleton.getSingleton();
+
+    System.out.println(instance1 == instance2);
+
+    System.out.println(instance1.equals(instance2));
+
+  }
+}
