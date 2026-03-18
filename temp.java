e56a154a547b038c515a96f07ab41a416e75e654
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2026-03-18 23:17:40 +0530

    "TEMP"

--- temp.java
+++ temp.java
@@ -9,12 +9,6 @@ public class temp {
     }
   }
   public static void main(String[] args) {
-    Singleton s1 = Singleton.getInstance();
-    Singleton s2 = Singleton.getInstance();
-
-    System.out.println(s1 == s2); 
-    System.out.println(s1.equals(s2));
-
-    System.out.println(s1.hashCode());    System.out.println(s2.hashCode());
+    
   }
 }
