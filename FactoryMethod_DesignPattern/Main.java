3ca67e73f09d9a9b35231fa14ab6fdf12e39dbd7
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2026-03-18 23:14:20 +0530

    "Today's Push"

--- FactoryMethod_DesignPattern/Main.java
+++ FactoryMethod_DesignPattern/Main.java
@@ -3,7 +3,7 @@ package FactoryMethod_DesignPattern;
 public class Main {
   public static void main(String[] args) {
     ILoggerFactory factory = new ErrorFactory();
-    ILogger errorE =  factory.createLogger();
-    errorE.log("Hey i am error logger");
+    ILogger error =  factory.createLogger();
+    error.log("Hey i am error logger");
   }
 }
